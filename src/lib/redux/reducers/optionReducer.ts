8d837f29923b3a1ae1fdc1690c4acc4f65c1652e
Hoang Minh Duc <duchm.bhsoft@gmail.com>
2024-03-06 14:15:07 +0700

    fix(view): add  initial state

--- src/lib/redux/reducers/optionReducer.ts
+++ src/lib/redux/reducers/optionReducer.ts
@@ -1,25 +1,7 @@
+import { defaultOption } from "../../config/defaultOpion";
 import { OptionState } from "../../modal/option";
 
-const INITIAL_STATE: OptionState[] = [
-  {
-    number: 1,
-    title: "Single",
-    subtitle: "Standaed price",
-    label: "",
-    quantity: 1,
-    discountType: "None",
-    amount: 0,
-  },
-  {
-    number: 2,
-    title: "Duo",
-    subtitle: "Save 10%",
-    label: "Popular",
-    quantity: 2,
-    discountType: "None",
-    amount: 0,
-  },
-];
+const INITIAL_STATE: OptionState[] = defaultOption;
 
 const optionReducer = (state = INITIAL_STATE, action: any) => {
   switch (action.type) {
